146034ad3d51c75499adbb4e012de51d1c545b08
Lauryn <ltrouill@ifremer.fr>
2024-10-18 13:09:16 +0000

    Modification mémoire et ncpus

--- scripts/illumina/Karlodinium_trinity.sh
+++ scripts/illumina/Karlodinium_trinity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N assembly
 #PBS -q omp
-#PBS -l ncpus=14
-#PBS -l mem=120gb
+#PBS -l ncpus=20
+#PBS -l mem=300gb
 #PBS -l walltime=48:00:00
 
 cd ${PBS_O_WORKDIR}
@@ -19,14 +19,14 @@ mkdir -p $LOG_FOLDER
 
 LEFT_READS=$(ls ${CHEMIN}data/rawdata/illumina/Karlodinium_karmit/*_R1.fastq.gz | tr '\n' ',')
 RIGHT_READS=$(ls ${CHEMIN}data/rawdata/illumina/Karlodinium_karmit//*_R2.fastq.gz | tr '\n' ',')
-0
+
 LEFT_READS=${LEFT_READS%,}
 RIGHT_READS=${RIGHT_READS%,}
 
 
 seqtype="--seqType fq"
-mem="--max_memory 100G"
-cpu="--CPU 28"
+mem="--max_memory 280G"
+cpu="--CPU 20"
 mincontiglength="--min_contig_length 200"
 output="--output ${CHEMIN}/results/illumina/Karlodinium_trinity/"
 cleanup="--full_cleanup"
