76e52726f18d9d502d2cf984a0e06d16be1c13fd
Lauryn <ltrouill@ifremer.fr>
2024-10-30 14:22:38 +0000

    Ajout commentaires

--- scripts/illumina/Karlodinium_trinity.sh
+++ scripts/illumina/Karlodinium_trinity.sh
@@ -5,6 +5,7 @@
 #PBS -l mem=300gb
 #PBS -l walltime=48:00:00
 
+# Chargement de l'environnement
 cd ${PBS_O_WORKDIR}
 
 . /appli/bioinfo/trinity/2.8.5/env.sh
@@ -18,12 +19,13 @@ LOG_FOLDER="${CHEMIN}/errors"
 mkdir -p $LOG_FOLDER
 
 LEFT_READS=$(ls ${CHEMIN}data/rawdata/illumina/Karlodinium_karmit/*_R1.fastq.gz | tr '\n' ',')
-RIGHT_READS=$(ls ${CHEMIN}data/rawdata/illumina/Karlodinium_karmit//*_R2.fastq.gz | tr '\n' ',')
+RIGHT_READS=$(ls ${CHEMIN}data/rawdata/illumina/Karlodinium_karmit/*_R2.fastq.gz | tr '\n' ',')
 
+# Retrait de la virgule
 LEFT_READS=${LEFT_READS%,}
 RIGHT_READS=${RIGHT_READS%,}
 
-
+# Paramètres de la commande Trinity
 seqtype="--seqType fq"
 mem="--max_memory 280G"
 cpu="--CPU 20"
@@ -31,7 +33,7 @@ mincontiglength="--min_contig_length 200"
 output="--output ${CHEMIN}/results/illumina/Karlodinium_trinity/"
 cleanup="--full_cleanup"
 
-
+# Commande trinity
 Trinity $seqtype $mem \
         --left $LEFT_READS \
         --right $RIGHT_READS \
