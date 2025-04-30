7d7c9dd297bce9af40fa56c8fcf729fb5e3a71e7
Lauryn Trouillot <lauryn3003@outlook.fr>
2025-04-30 14:32:53 +0200

    Retrait des transcripts avec une faible expression selon les SR

--- Scripts/Custom/Prymnesium/Assembly_cleaning/low_expression.sh
+++ Scripts/Custom/Prymnesium/Assembly_cleaning/low_expression.sh
@@ -17,9 +17,6 @@ SAMPLES=("AA")  # adapter si plusieurs échantillons
 EXT_R1="_R1.cleaned.fastq.gz"
 EXT_R2="_R2.cleaned.fastq.gz"
 
-LONG_READ_DIR="/home/datawork-lpba/Prymnesium/PrymneTranscripto/RNA-longReads-fev25/"
-LONG_READ_EXT="Galaxy43_Prymnesium_cDNA_Porechop_Qualite.fastq"
-
 # Seuils de filtrage
 MIN_EXPR=1
 MIN_DOM_ISO=50
@@ -30,7 +27,7 @@ WORKDIR="/home1/scratch/ltrouill/${TRANS_NAME}_analysis_$(date +%Y%m%d_%H%M%S)"
 mkdir -p "$WORKDIR"
 cd "$WORKDIR"
 
-echo "=== Début du pipeline RATTLE - $(date +%Y%m%d_%H%M%S) ===" > "$LOGFILE"
+echo "=== Début du pipeline transcriptome - $(date +%Y%m%d_%H%M%S) ===" > "$LOGFILE"
 
 # === Indexing transcriptome ===
 . /appli/bioinfo/salmon/1.10.0/env.sh
@@ -39,47 +36,21 @@ if [ ! -d "$WORKDIR/salmon_index" ]; then
     salmon index -t "$TRANSCRIPTS" -i "$WORKDIR/salmon_index" 2>> "$LOGFILE"
 fi
 
-# === Quantification SR et/ou LR ===
+# === Quantification Short Reads uniquement ===
 mkdir -p "$WORKDIR/salmon_quant"
 
 for SAMPLE in "${SAMPLES[@]}"; do
-    HAS_SR=1
-    HAS_LR=1
-
     R1="${READS_DIR}/${SAMPLE}${EXT_R1}"
     R2="${READS_DIR}/${SAMPLE}${EXT_R2}"
-    LR="${LONG_READ_DIR}/${LONG_READ_EXT}"
 
-    if [[ -s "$R1" && -s "$R2" ]]; then HAS_SR=1; fi
-    if [[ -s "$LR" ]]; then HAS_LR=1; fi
-
-    if (( HAS_SR )); then
+    if [[ -s "$R1" && -s "$R2" ]]; then
         echo "[+] Quantifying SR for sample $SAMPLE..." >> "$LOGFILE"
         salmon quant -i "$WORKDIR/salmon_index" -l A \
             -1 "$R1" -2 "$R2" \
             -p 8 --validateMappings \
             -o "$WORKDIR/salmon_quant/${SAMPLE}_SR_quant" 2>> "$LOGFILE"
-    fi
-
-    if (( HAS_LR )); then
-        echo "[+] Aligning LR with minimap2..." >> "$LOGFILE"
-        . /appli/bioinfo/minimap2/2.28/env.sh
-        minimap2 -ax splice "$TRANSCRIPTS" "$LR" > "$WORKDIR/${SAMPLE}_LR.sam"
-
-        echo "[+] Converting SAM to sorted BAM..." >> "$LOGFILE"
-        . /appli/bioinfo/samtools/1.9/env.sh
-        samtools view -@ 8 -bS "$WORKDIR/${SAMPLE}_LR.sam" | \
-            samtools sort -@ 8 -o "$WORKDIR/${SAMPLE}_LR.bam"
-        samtools index "$WORKDIR/${SAMPLE}_LR.bam"
-        rm "$WORKDIR/${SAMPLE}_LR.sam"
-
-        echo "[+] Quantifying LR (alignment-based)..." >> "$LOGFILE"
-        . /appli/bioinfo/salmon/1.10.0/env.sh
-        salmon quant -t "$TRANSCRIPTS" \
-                     -l A \
-                     -a "$WORKDIR/${SAMPLE}_LR.bam" \
-                     -p 8 \
-                     -o "$WORKDIR/salmon_quant/${SAMPLE}_LR_quant" 2>> "$LOGFILE"
+    else
+        echo "[!] Fichiers manquants pour $SAMPLE : $R1 ou $R2" >> "$LOGFILE"
     fi
 done
 
@@ -96,8 +67,6 @@ echo "[+] Generating TPM matrix..." >> "$LOGFILE"
 
 QUANT_LIST="$WORKDIR/quant_files_list.txt"
 find "$WORKDIR/salmon_quant" -name "quant.sf" > "$QUANT_LIST"
-
-
 
 singularity run -B $DATAWORK,$SCRATCH ${TRINITY_IMG} /usr/local/bin/util/abundance_estimates_to_matrix.pl \
     --est_method salmon \
