93e14db49c604c955dbd64358654e6df718a9196
Lauryn Trouillot <lauryn3003@outlook.fr>
2025-04-30 14:34:00 +0200

    Clusterisation rnaspades SR AA + LR + low_expression 0.95

--- Scripts/Custom/Prymnesium/Assembly_cleaning/cdhitest.sh
+++ Scripts/Custom/Prymnesium/Assembly_cleaning/cdhitest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -N cluster
 #PBS -q omp
-#PBS -l ncpus=20
-#PBS -l mem=300gb
-#PBS -l walltime=300:00:00
+#PBS -l ncpus=15
+#PBS -l mem=100gb
+#PBS -l walltime=10:00:00
 
 # Chargement de l'environnement
 cd "${PBS_O_WORKDIR}"
@@ -11,7 +11,7 @@ cd "${PBS_O_WORKDIR}"
 # Variables
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 NAME="cluster_${TIMESTAMP}"
-ASSEMBLY_FILE="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/low_expression/transcripts_analysis_20250424_212355/filtered_transcripts.fasta"
+ASSEMBLY_FILE="/home1/scratch/ltrouill/transcripts_analysis_20250430_064812/filtered_transcripts.fasta"
 LOG_FOLDER="/home1/datawork/ltrouill/Ifremer/Errors/Clustering/"
 LOG_FILE="$LOG_FOLDER/${NAME}.log"
 RESULT_FOLDER="/home1/scratch/ltrouill/${NAME}"
@@ -19,7 +19,7 @@ RESULT_FOLDER="/home1/scratch/ltrouill/${NAME}"
 mkdir -p "$LOG_FOLDER" "$RESULT_FOLDER" 
 
 THRESHOLD_VALUE=10
-SIMILARITY_VALUE=0.98
+SIMILARITY_VALUE=0.95
 
 CLUSTER_FILE="$RESULT_FOLDER/cluster_${SIMILARITY_VALUE}_${THRESHOLD_VALUE}.fasta"
 
