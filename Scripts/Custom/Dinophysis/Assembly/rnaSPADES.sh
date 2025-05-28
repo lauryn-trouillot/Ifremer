6a2b625ccb5fbcaab7760bfde01d789695345cf8
Lauryn Trouillot <lauryn3003@outlook.fr>
2025-05-28 14:37:41 +0200

    Assemblage du transcriptome de dinophysis

--- Scripts/Custom/Dinophysis/Assembly/rnaSPADES.sh
+++ Scripts/Custom/Dinophysis/Assembly/rnaSPADES.sh
@@ -14,8 +14,8 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 NAME="rnaspades_${TIMESTAMP}"
 LOG_FOLDER="/home1/datawork/ltrouill/Ifremer/Errors/rnaspades"
 RESULT_FOLDER="/home1/scratch/ltrouill/${NAME}"
-READS_FOLDER="/home1/datawork/ltrouill/Ifremer/Data/Cleaned_data/Karlodinium/Short_reads/fastp_20250414_121346"
-MINION_FILE="/home1/datawork/ltrouill/Ifremer/Data/Cleaned_data/Karlodinium/Long_reads/porechop_20250416/Karlodinium_cDNA_cleaned.fastq"
+READS_FOLDER="/home1/datawork/ltrouill/Ifremer/Data/Cleaned_data/Dinophysis/fastp_20250527_124206"
+MINION_FILE="/home1/datawork/ltrouill/Ifremer/Data/Cleaned_data/Dinophysis/DiNoPrey.fastq.gz"
 
 # Paramètres SPAdes
 SPADES_THREADS=20
@@ -25,11 +25,11 @@ SPADES_MEMORY=300
 mkdir -p "$LOG_FOLDER" "$RESULT_FOLDER"
 
 # Combiner les fichiers de lecture Illumina
-for file in "$READS_FOLDER"/9_R1.cleaned.fastq.gz; do
+for file in "$READS_FOLDER"/ERR9832999_R1.cleaned.fastq.gz; do
     cat "$file" >> "$RESULT_FOLDER/LEFT.fastq.gz"
 done
 
-for file in "$READS_FOLDER"/9_R2.cleaned.fastq.gz; do
+for file in "$READS_FOLDER"/ERR9832999_R2.cleaned.fastq.gz; do
     cat "$file" >> "$RESULT_FOLDER/RIGHT.fastq.gz"
 done
 
