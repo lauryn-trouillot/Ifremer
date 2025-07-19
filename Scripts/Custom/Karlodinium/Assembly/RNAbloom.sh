f3942a0154f07811ea4f6df00f843d444a874208
Lauryn Trouillot <lauryn3003@outlook.fr>
2025-07-19 16:02:15 +0200

    Relancement rnabloom sur Karlo

---
+++ Scripts/Custom/Karlodinium/Assembly/RNAbloom.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#PBS -N rnabloom_L
+#PBS -q omp
+#PBS -l ncpus=16
+#PBS -l mem=300gb
+#PBS -l walltime=300:00:00
+
+# Chargement de l'environnement
+cd "${PBS_O_WORKDIR}"
+. /appli/bioinfo/rnabloom/2.0.1/env.sh
+
+# Variables
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+NAME="rnabloom_${TIMESTAMP}"
+FILENAME="/home1/datawork/ltrouill/Ifremer/Data/Cleaned_data/Karlodinium/Long_reads/fastplong_20250516_074145/Karlodinium_cleaned.fastq"
+RESULT_FOLDER="/home1/scratch/ltrouill/rnabloom_${TIMESTAMP}"
+LOG_FILE="$RESULT_FOLDER/${NAME}.log"
+
+# Paramètres RNAbloom
+THREADS=16
+MEMORY=300
+
+
+mkdir -p "$LOG_FOLDER" "$RESULT_FOLDER"
+
+# Exécution de RNAbloom
+rnabloom -long "$FILENAME" \
+            -t $THREADS \
+            -mem $MEMORY \
+            -outdir "$RESULT_FOLDER" >> "$LOG_FILE" 2>&1
+
