1afc275975f44f5d031aae844c69fbe4647d31e2
Lauryn Trouillot <lauryn3003@outlook.fr>
2025-07-19 16:03:09 +0200

    Ajout de la db 5.8S

--- Scripts/Custom/Prymnesium/Data_cleaning/blast.sh
+++ Scripts/Custom/Prymnesium/Data_cleaning/blast.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #PBS -N blast_rrna
 #PBS -l ncpus=4
@@ -12,11 +11,12 @@ cd $PBS_O_WORKDIR
 . /appli/bioinfo/blast/2.12.0/env.sh
 
 # Chemins en dur
-TRANSCRIPTS="/home/datawork-lpba/Prymnesium/PrymneTranscripto/TranscriptomeRef-Genalg/LongReadsRNA/CleanedData/Galaxy43_Prymnesium_cDNA_Porechop_Qualite.fastq"
-PREFIX="rawLR"
+TRANSCRIPTS="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/rnabloom/rnabloom_20250425_085518/rnabloom.transcripts.fasta"
+PREFIX="rnabloom"
 
 DB_18S="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/blast/blastdb/18S_fungal_sequences/18S_fungal_sequences"
 DB_28S="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/blast/blastdb/28S_fungal_sequences/28S_fungal_sequences"
+DB_58S="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/blast/blastdb/5_8S_sequences/5_8_rRNA_db"
 
 RESULTS_FOLDER="/home1/datawork/ltrouill/Ifremer/Results/Prymnesium/blast/blast_$(date +%Y%m%d_%H%M%S)"
 
@@ -24,12 +24,15 @@ mkdir -p "$RESULTS_FOLDER"
 
 OUT_18S="$RESULTS_FOLDER/${PREFIX}_vs_18S.txt"
 OUT_28S="$RESULTS_FOLDER/${PREFIX}_vs_28S.txt"
+OUT_58S="$RESULTS_FOLDER/${PREFIX}_vs_58S.txt"
 CANDIDATES_18S="$RESULTS_FOLDER/${PREFIX}_18S_candidates.txt"
 CANDIDATES_28S="$RESULTS_FOLDER/${PREFIX}_28S_candidates.txt"
+CANDIDATES_58S="$RESULTS_FOLDER/${PREFIX}_58S_candidates.txt"
 ALL_RRNA="$RESULTS_FOLDER/${PREFIX}_rRNA_candidates.txt"
 SUMMARY="$RESULTS_FOLDER/${PREFIX}_rRNA_summary.txt"
 LOG_FILE="$RESULTS_FOLDER/blast_$(date +%Y%m%d_%H%M%S).log"
 
+### Recherche des ARNr 18S 
 echo "[`$(date)`] BLASTN vs 18S..." > $LOG_FILE
 blastn -query "$TRANSCRIPTS" -db "$DB_18S" -out "$OUT_18S" -evalue 1e-20 -outfmt 6 -num_threads 4 2>> "$LOG_FILE"
 
@@ -37,28 +40,38 @@ echo "[`date`] Extraction des identifiants 18S..." >> $LOG_FILE
 
 cut -f1 "$OUT_18S" | sort | uniq > "$CANDIDATES_18S"
 
+### Recherche des ARNr 28S
 echo "[`date`] BLASTN vs 28S..." >> $LOG_FILE
-
 blastn -query "$TRANSCRIPTS" -db "$DB_28S" -out "$OUT_28S" -evalue 1e-20 -outfmt 6 -num_threads 4 2>> "$LOG_FILE"
-
 
 echo "[`date`] Extraction des identifiants 28S...">> $LOG_FILE
 
 cut -f1 "$OUT_28S" | sort | uniq > "$CANDIDATES_28S"
 
+### Recherche des ARNr 5.8S
+echo "[`date`] BLASTN vs 5.8S..." >> $LOG_FILE
+blastn -query "$TRANSCRIPTS" -db "$DB_58S" -out "$OUT_58S" -evalue 1e-20 -outfmt 6 -num_threads 4 2>> "$LOG_FILE"
+
+echo "[`date`] Extraction des identifiants 5.8S...">> $LOG_FILE
+
+cut -f1 "$OUT_58S" | sort | uniq > "$CANDIDATES_58S"
+
+
 echo "[`date`] Fusion des candidats ARNr...">> $LOG_FILE
 
-cat "$CANDIDATES_18S" "$CANDIDATES_28S" | sort | uniq > "$ALL_RRNA"
+cat "$CANDIDATES_18S" "$CANDIDATES_28S" "$CANDIDATES_58S" | sort | uniq > "$ALL_RRNA"
 
 # Résumé
 TOTAL=$(grep -c "^>" "$TRANSCRIPTS")
 # TOTAL=$(($(wc -l < "$TRANSCRIPTS") / 4)) Pour les reads brute fastq
 N_18S=$(wc -l < "$CANDIDATES_18S")
 N_28S=$(wc -l < "$CANDIDATES_28S")
+N_58S=$(wc -l < "$CANDIDATES_58S")
 N_TOTAL=$(wc -l < "$ALL_RRNA")
 
 P_18S=$(awk -v n=$N_18S -v t=$TOTAL 'BEGIN {printf "%.2f", (n/t)*100}')
 P_28S=$(awk -v n=$N_28S -v t=$TOTAL 'BEGIN {printf "%.2f", (n/t)*100}')
+P_58S=$(awk -v n=$N_58S -v t=$TOTAL 'BEGIN {printf "%.2f", (n/t)*100}')
 P_TOTAL=$(awk -v n=$N_TOTAL -v t=$TOTAL 'BEGIN {printf "%.2f", (n/t)*100}')
 
 echo "[`date`] Résumé écrit dans $SUMMARY"
@@ -68,7 +81,8 @@ Nombre total de transcripts : $TOTAL
 
 > ARNr 18S : $N_18S transcripts (${P_18S}%)
 > ARNr 28S : $N_28S transcripts (${P_28S}%)
-> Total ARNr uniques (18S + 28S) : $N_TOTAL transcripts (${P_TOTAL}%)
+> ARNr 5.8S : $N_58S transcripts (${P_58S}%)
+> Total ARNr uniques (18S + 28S + 5.8S) : $N_TOTAL transcripts (${P_TOTAL}%)
 EOF
 
 cat "$SUMMARY"
